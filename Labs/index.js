a8f814716f057c12ac490a61da149add2af2d254
Sopan001 <65335771+Sopan001@users.noreply.github.com>
2021-05-10 14:16:30 +0530

    Homepag,Lab name page update

---
+++ Labs/index.js
@@ -0,0 +1,8 @@
+var menu = document.querySelector('.menu');
+var menuBtn = document.querySelector('.menu button');
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('opened')
+})
+
+
+
